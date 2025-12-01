87ddd475c0927382e47a01ff67970bd425d70665
SINISTAR\rldun <ray.l.dunn@gmail.com>
2025-12-01 12:32:34 -0800

    add clef-cli

---
+++ cli/__init__.py
@@ -0,0 +1,17 @@
+from .clef_cli import (
+    setup_argparser,
+    validate_config_paths,
+    load_configs,
+    validate_configs,
+    run_experiment,
+    main,
+)
+
+__all__ = [
+    'setup_argparser',
+    'validate_config_paths',
+    'load_configs',
+    'validate_configs',
+    'run_experiment',
+    'main',
+]
